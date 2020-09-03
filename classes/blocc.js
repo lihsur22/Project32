028e132c759058b28290fb879d62708c096d182c
lihsur22 <rushilganguli@outlook.com>
2020-09-03 17:42:12 +0530

    Vanishing Blocks

--- classes/blocc.js
+++ classes/blocc.js
@@ -11,6 +11,7 @@ class Blocc{
         this.c = c;
         this.body = Bodies.rectangle(this.x,this.y,30,40,options);
         World.add(world,this.body);
+        this.visiblity = 255;
     }
 
 
@@ -18,31 +19,39 @@ class Blocc{
     display(){
         var pos = this.body.position;
         var col = this.c
+        
+        if(this.body.speed < 3) 
+        {
+            push();
+            translate(pos.x,pos.y);
+            rotate(this.body.angle);
 
-        push();
-        translate(pos.x,pos.y);
-        rotate(this.body.angle);
+            switch(col){
+                case 1:
+                    fill("#86CDE9");
+                    break;
+                case 2:
+                    fill("#FEBFCA");
+                    break;
+                case 3:
+                    fill("#3EDFCF");
+                    break;
+                case 4:
+                    fill("#7F7F7F");
+                    break;
+                default:
+                    break;
+            }
 
-        switch(col){
-            case 1:
-                fill("#86CDE9");
-                break;
-            case 2:
-                fill("#FEBFCA");
-                break;
-            case 3:
-                fill("#3EDFCF");
-                break;
-            case 4:
-                fill("#7F7F7F");
-                break;
-            default:
-                break;
+            strokeWeight(2);
+            rectMode(CENTER);
+            rect(0, 0, 30, 40);
+            pop();
+        } else 
+        {
+            World.remove(world, this.body);
+            push();
+            pop();
         }
-
-        strokeWeight(2);
-        rectMode(CENTER);
-        rect(0, 0, 30, 40);
-        pop();
     }
 };
